d9531ccf632b51cd6f5908fe07d54072dd607ff8
Weslley_Palhares <Weslley_Palhares@DESKTOP-NO2OI1L>
2024-07-13 19:53:32 -0300

    Criação das classes: Acesso, Pessoa, PessoaFisica, PessoaJuridica e Endereço

---
+++ src/main/java/jdev/mentoria/lojavirtual/model/Acesso.java
@@ -0,0 +1,67 @@
+package jdev.mentoria.lojavirtual.model;
+
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+import org.springframework.security.core.GrantedAuthority;
+
+@Entity
+@Table(name = "acesso")
+@SequenceGenerator(name = "seq_acesso", sequenceName = "seq_acesso", initialValue = 1, allocationSize = 1)
+public class Acesso implements GrantedAuthority {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_acesso")
+	private Long id;
+
+	@Column(nullable = false)
+	private String descricao;/* Acesso ex: ROLE_ADMIN ou ROLE_SECRETARIO */
+
+	@Override
+	public String getAuthority() {
+		return this.descricao;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Acesso other = (Acesso) obj;
+		return Objects.equals(id, other.id);
+	}
+
+}
