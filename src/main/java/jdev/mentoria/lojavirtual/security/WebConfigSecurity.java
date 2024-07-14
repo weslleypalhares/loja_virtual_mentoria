0cb1f86580c730292d2c71cb142c9339f54152d4
Weslley_Palhares <Weslley_Palhares@DESKTOP-NO2OI1L>
2024-07-14 12:55:13 -0300

    Criação do pacote Security Criação da classe WebConfigSecurity

---
+++ src/main/java/jdev/mentoria/lojavirtual/security/WebConfigSecurity.java
@@ -0,0 +1,24 @@
+package jdev.mentoria.lojavirtual.security;
+
+import javax.servlet.http.HttpSessionListener;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
+public class WebConfigSecurity extends WebSecurityConfigurerAdapter implements HttpSessionListener{
+	
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers(HttpMethod.GET, "/salvarAcesso", "/deleteAcesso")
+		.antMatchers(HttpMethod.POST, "/salvarAcesso", "/deleteAcesso");
+		/*Ignorando URL no momento para não autenticar*/
+	}
+
+}
--- src/test/java/jdev/mentoria/lojavirtual/LojaVirtualMentoriaApplicationTests.java
+++ src/test/java/jdev/mentoria/lojavirtual/LojaVirtualMentoriaApplicationTests.java
@@ -1,5 +1,7 @@
 package jdev.mentoria.lojavirtual;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -8,13 +10,17 @@ import jdev.mentoria.lojavirtual.controller.AcessoController;
 import jdev.mentoria.lojavirtual.model.Acesso;
 import jdev.mentoria.lojavirtual.repository.AcessoRepository;
 import jdev.mentoria.lojavirtual.service.AcessoService;
+import junit.framework.TestCase;
 
 @SpringBootTest(classes = LojaVirtualMentoriaApplication.class)
-public class LojaVirtualMentoriaApplicationTests {
+public class LojaVirtualMentoriaApplicationTests extends TestCase{
 
 	
 	@Autowired
 	private AcessoController acessoController;
+	
+	@Autowired
+	private AcessoRepository acessoRepository;
 	
 	@Test
 	public void testCadastraAcesso() {
@@ -23,7 +29,48 @@ public class LojaVirtualMentoriaApplicationTests {
 		
 		acesso.setDescricao("ROLE_ADMIN");
 		
-		acessoController.salvarAcesso(acesso);
+		assertEquals(true, acesso.getId() == null);
+		
+		/*Gravou no banco de dados*/
+		acesso = acessoController.salvarAcesso(acesso).getBody();
+		
+		assertEquals(true, acesso.getId() > 0);
+		
+		/*Validar dados salvos da forma correta*/
+		assertEquals("ROLE_ADMIN", acesso.getDescricao());
+		
+		/*Teste de Carregamento*/
+		
+		Acesso acesso2 = acessoRepository.findById(acesso.getId()).get();
+		
+		
+		assertEquals(acesso.getId(), acesso2.getId());
+		
+		/*Teste de delete*/
+		
+		acessoRepository.deleteById(acesso2.getId());
+		
+		
+		acessoRepository.flush(); /*Roda esse SQL de delete no banco de dados*/
+		
+		Acesso acesso3 = acessoRepository.findById(acesso2.getId()).orElse(null);
+		
+		assertEquals(true, acesso3 == null);
+		
+		
+		/*Teste de Query*/
+		
+		acesso = new Acesso();
+		
+		acesso.setDescricao("ROLE_ALUNO");
+		
+		acesso = acessoController.salvarAcesso(acesso).getBody();
+		
+		List<Acesso> acessos = acessoRepository.buscarAcessoDesc("ALUNO".trim().toUpperCase());
+		
+		assertEquals(1, acessos.size());
+		
+		acessoRepository.deleteById(acesso.getId());
 		
 	}
 
