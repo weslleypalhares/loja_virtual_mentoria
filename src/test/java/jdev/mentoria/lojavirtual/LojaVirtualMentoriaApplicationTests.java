aa93d18d93329dd3b987af6d5f2ea2f2a23b5f37
Weslley_Palhares <Weslley_Palhares@DESKTOP-NO2OI1L>
2024-07-14 11:36:39 -0300

    Adicionando o SQL completo do Banco de Dados no V0_init.sql
    Criação dos Pacotes Service, controller e repository
    
    Configuração do application.properties
    
    Criação do AcessoRepository, AcessoService, AcessoController

--- src/test/java/jdev/mentoria/lojavirtual/LojaVirtualMentoriaApplicationTests.java
+++ src/test/java/jdev/mentoria/lojavirtual/LojaVirtualMentoriaApplicationTests.java
@@ -1,13 +1,30 @@
 package jdev.mentoria.lojavirtual;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class LojaVirtualMentoriaApplicationTests {
+import jdev.mentoria.lojavirtual.controller.AcessoController;
+import jdev.mentoria.lojavirtual.model.Acesso;
+import jdev.mentoria.lojavirtual.repository.AcessoRepository;
+import jdev.mentoria.lojavirtual.service.AcessoService;
 
+@SpringBootTest(classes = LojaVirtualMentoriaApplication.class)
+public class LojaVirtualMentoriaApplicationTests {
+
+	
+	@Autowired
+	private AcessoController acessoController;
+	
 	@Test
-	void contextLoads() {
+	public void testCadastraAcesso() {
+		
+		Acesso acesso = new Acesso();
+		
+		acesso.setDescricao("ROLE_ADMIN");
+		
+		acessoController.salvarAcesso(acesso);
+		
 	}
 
 }
