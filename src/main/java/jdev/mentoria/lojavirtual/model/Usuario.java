76f1e2a94c832120279c8bffb12cc1a3ea3f3672
Weslley_Palhares <Weslley_Palhares@DESKTOP-NO2OI1L>
2024-07-14 00:03:41 -0300

    Criado o ENUM: StatusContaReceber StatusContaPagar
    Criado as classes:
    ContaPagar
    ContaReceber
    FormaPagamento

--- src/main/java/jdev/mentoria/lojavirtual/model/Usuario.java
+++ src/main/java/jdev/mentoria/lojavirtual/model/Usuario.java
@@ -44,12 +44,16 @@ public class Usuario implements UserDetails{
 	private Date dataAtualSenha;
 	
 	@OneToMany(fetch = FetchType.LAZY)
-	@JoinTable(name = "usuarios_acesso", uniqueConstraints = @UniqueConstraint (columnNames = {"usuario_id", "acesso_id"} ,
-	  name = "unique_acesso_user"),
-	  joinColumns = @JoinColumn(name = "usuario_id", referencedColumnName = "id", table = "usuario", 
-	  unique = false, foreignKey = @ForeignKey(name = "usuario_fk", value = ConstraintMode.CONSTRAINT)), 
-	  inverseJoinColumns = @JoinColumn(name = "acesso_id", unique = false, referencedColumnName = "id", table = "acesso",
-	  foreignKey = @ForeignKey(name = "acesso_fk", value = ConstraintMode.CONSTRAINT)))
+	@JoinTable(name = "usuarios_acesso", 
+			uniqueConstraints = @UniqueConstraint (columnNames = {"usuario_id", "acesso_id"} ,
+			name = "unique_acesso_user"),
+			
+			joinColumns = @JoinColumn(name = "usuario_id", referencedColumnName = "id", table = "usuario", 
+			unique = false, foreignKey = @ForeignKey(name = "usuario_fk", value = ConstraintMode.CONSTRAINT)), 
+			
+			inverseJoinColumns = @JoinColumn(name = "acesso_id", 
+	  		unique = false, referencedColumnName = "id", table = "acesso",
+	  		foreignKey = @ForeignKey(name = "acesso_fk", value = ConstraintMode.CONSTRAINT)))
 	private List<Acesso> acessos;
 	
 	/*Autoridades = São os Acessos, ou seja ROLE_ADMIN, ROLE_SECRETARIO, ROLE_FINANCEIRO*/
