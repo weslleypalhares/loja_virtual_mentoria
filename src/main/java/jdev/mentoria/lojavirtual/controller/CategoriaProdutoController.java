6a60484c338917b5db7497e17236aabb2e77e957
Weslley_Palhares <Weslley_Palhares@DESKTOP-NO2OI1L>
2024-07-22 20:53:45 -0300

    Criado a interface: CategoriaProdutoRepository
    Criado a classe:
    CategoriaProdutoController
    CategoriaProdutoDto

---
+++ src/main/java/jdev/mentoria/lojavirtual/controller/CategoriaProdutoController.java
@@ -0,0 +1,69 @@
+package jdev.mentoria.lojavirtual.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import jdev.mentoria.lojavirtual.ExceptionMentoriaJava;
+import jdev.mentoria.lojavirtual.model.CategoriaProduto;
+import jdev.mentoria.lojavirtual.model.dto.CategoriaProdutoDto;
+import jdev.mentoria.lojavirtual.repository.CategoriaProdutoRepository;
+
+@RestController
+public class CategoriaProdutoController {
+
+	@Autowired
+	private CategoriaProdutoRepository categoriaProdutoRepository;
+	
+	
+	@ResponseBody
+	@GetMapping(value = "**/buscarPorDescCategoria/{desc}")
+	public ResponseEntity<List<CategoriaProduto>> buscarPorDesc(@PathVariable("desc") String desc) { 
+		
+		List<CategoriaProduto> acesso = categoriaProdutoRepository.buscarCategoriaDes(desc.toUpperCase());
+		
+		return new ResponseEntity<List<CategoriaProduto>>(acesso,HttpStatus.OK);
+	}
+	
+	
+	@ResponseBody /*Poder dar um retorno da API*/
+	@PostMapping(value = "**/deleteCategoria") /*Mapeando a url para receber JSON*/
+	public ResponseEntity<?> deleteAcesso(@RequestBody CategoriaProduto CategoriaProduto) { /*Recebe o JSON e converte pra Objeto*/
+		
+		categoriaProdutoRepository.deleteById(CategoriaProduto.getId());
+		
+		return new ResponseEntity("Categoria Removida",HttpStatus.OK);
+	}
+	
+	
+	@ResponseBody
+	@PostMapping(value = "**/salvarCategoria")
+	public ResponseEntity<CategoriaProdutoDto> salvarCategoria(@RequestBody CategoriaProduto categoriaProduto) throws ExceptionMentoriaJava{
+		
+		if (categoriaProduto.getEmpresa()  == null || (categoriaProduto.getEmpresa().getId() == null)) {
+			throw new ExceptionMentoriaJava("A empresa deve ser informaada.");
+		}
+		
+		if (categoriaProduto.getId() == null && categoriaProdutoRepository.existeCategoria(categoriaProduto.getNomeDesc())) {
+			throw new ExceptionMentoriaJava("Não pode cadastrar categoria com o mesmo nome.");
+		}
+		
+		CategoriaProduto categoriaSalva = categoriaProdutoRepository.save(categoriaProduto);
+		
+		CategoriaProdutoDto categoriaProdutoDto = new CategoriaProdutoDto();
+		categoriaProdutoDto.setId(categoriaSalva.getId());
+		categoriaProdutoDto.setNomeDesc(categoriaSalva.getNomeDesc());
+		categoriaProdutoDto.setEmpresa(categoriaSalva.getEmpresa().getId().toString());
+		
+		
+		return new ResponseEntity<CategoriaProdutoDto>(categoriaProdutoDto, HttpStatus.OK);
+	}
+}
