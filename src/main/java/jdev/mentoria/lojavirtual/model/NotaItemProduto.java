fc5dd5ec9068e601ab6b96488f85f0b139871611
Weslley_Palhares <Weslley_Palhares@DESKTOP-NO2OI1L>
2024-08-08 22:59:59 -0300

    08/08/2024 Criado: NotaItemProdutoController NotaItemProdutoRepository

--- src/main/java/jdev/mentoria/lojavirtual/model/NotaItemProduto.java
+++ src/main/java/jdev/mentoria/lojavirtual/model/NotaItemProduto.java
@@ -14,6 +14,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
+import javax.validation.constraints.Size;
 
 
 @Entity
@@ -27,6 +28,7 @@ public class NotaItemProduto implements Serializable{
 	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_nota_item_produto")
 	private Long id;
 	
+	//@Size(min = 1, message = "Informe a quantidade do produto")
 	@Column(nullable = false)
 	private Double quantidade;
 	
@@ -44,15 +46,15 @@ public class NotaItemProduto implements Serializable{
 	@ManyToOne(targetEntity = Pessoa.class)
 	@JoinColumn(name = "empresa_id", nullable = false,
 	foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "empresa_id_fk"))
-	private Pessoa empresa;
+	private PessoaJuridica empresa;
 	
 	
 
-	public Pessoa getEmpresa() {
+	public PessoaJuridica getEmpresa() {
 		return empresa;
 	}
 
-	public void setEmpresa(Pessoa empresa) {
+	public void setEmpresa(PessoaJuridica empresa) {
 		this.empresa = empresa;
 	}
 
