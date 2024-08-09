c8229363cab07b88aeaad5e97a7776e7ff61834d
Weslley_Palhares <Weslley_Palhares@DESKTOP-NO2OI1L>
2024-08-09 00:21:00 -0300

    09/08/2024 Salvando imagem em base64

--- src/main/java/jdev/mentoria/lojavirtual/model/Produto.java
+++ src/main/java/jdev/mentoria/lojavirtual/model/Produto.java
@@ -2,17 +2,22 @@ package jdev.mentoria.lojavirtual.model;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.ConstraintMode;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.ForeignKey;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
@@ -69,9 +74,9 @@ public class Produto implements Serializable{
 	private BigDecimal valorVenda = BigDecimal.ZERO;
 	
 	@Column(nullable = false)
-	private Integer QtdEstoque = 0;
+	private Integer qtdEstoque = 0;
 	
-	private Integer QtdeAlertaEstoque = 0;
+	private Integer qtdeAlertaEstoque = 0;
 	
 	private String linkYoutube;
 	
@@ -100,6 +105,17 @@ public class Produto implements Serializable{
 	private MarcaProduto marcaProduto = new MarcaProduto();
 	
 	
+	@OneToMany(mappedBy = "produto", orphanRemoval = true, cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+	private List<ImagemProduto> imagens = new ArrayList<ImagemProduto>();
+	
+	public void setImagens(List<ImagemProduto> imagens) {
+		this.imagens = imagens;
+	}
+	
+	
+	public List<ImagemProduto> getImagens() {
+		return imagens;
+	}
 	
 	public void setMarcaProduto(MarcaProduto marcaProduto) {
 		this.marcaProduto = marcaProduto;
@@ -205,21 +221,27 @@ public class Produto implements Serializable{
 		this.valorVenda = valorVenda;
 	}
 
+
+
 	public Integer getQtdEstoque() {
-		return QtdEstoque;
+		return qtdEstoque;
 	}
+
 
 	public void setQtdEstoque(Integer qtdEstoque) {
-		QtdEstoque = qtdEstoque;
+		this.qtdEstoque = qtdEstoque;
 	}
+
 
 	public Integer getQtdeAlertaEstoque() {
-		return QtdeAlertaEstoque;
+		return qtdeAlertaEstoque;
 	}
 
+
 	public void setQtdeAlertaEstoque(Integer qtdeAlertaEstoque) {
-		QtdeAlertaEstoque = qtdeAlertaEstoque;
+		this.qtdeAlertaEstoque = qtdeAlertaEstoque;
 	}
+
 
 	public String getLinkYoutube() {
 		return linkYoutube;
